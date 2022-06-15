2b0f1b66e4752291fc18e33bf7dec545fa14ec71
kenorb <kenorb@users.noreply.github.com>
2022-06-15 15:10:24 +0100

    Increases BATCH_MAX to 4096*10

--- brainflayer.c
+++ brainflayer.c
@@ -33,7 +33,7 @@
 #include "algo/sha3.h"
 
 // raise this if you really want, but quickly diminishing returns
-#define BATCH_MAX 4096*8
+#define BATCH_MAX 4096*10
 
 // Number of supported bloom files.
 #define BOPT_MAX 10
